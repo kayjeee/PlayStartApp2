5202e0e8fdbc9726565f51a437460fdf337f2e89
kayjeee <Kagiso.killagram@gmail.com>
2015-11-07 19:20:29 +0200

    Update Global.java

--- app/Global.java
+++ app/Global.java
@@ -18,17 +18,10 @@ public class Global extends GlobalSettings {
 	public void onStart(play.Application arg0) {
 		super.beforeStart(arg0);
 		Logger.debug("** onStart **"); 
-		try {
-			MorphiaObject.mongo = new Mongo("127.0.0.1", 27017);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		Configuration config = Configuration.root().getConfig("jongo");
-	      
-        String test = config.getString("mongodb.dbname");
-     
+
+      
 		MorphiaObject.morphia = new Morphia();
-		MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, test);
+	
 		MorphiaObject.datastore.ensureIndexes();   
 		MorphiaObject.datastore.ensureCaps();  
 
