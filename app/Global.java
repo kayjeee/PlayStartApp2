55049115dfc4c56bbf38f559dec8af688d3fc122
kayjeee <Kagiso.killagram@gmail.com>
2015-11-08 06:46:07 +0200

    Update Global.java

--- app/Global.java
+++ app/Global.java
@@ -9,19 +9,35 @@ import com.mongodb.Mongo;
 
 import play.Application;
 import play.Play;
+import java.net.UnknownHostException;
+
+import play.GlobalSettings;
+import play.Logger;
 import play.Configuration;
+import com.google.code.morphia.Morphia;
+import com.mongodb.Mongo;
+
 import controllers.MorphiaObject;
 
 public class Global extends GlobalSettings {
 
-@Override
+	@Override
 	public void onStart(play.Application arg0) {
 		super.beforeStart(arg0);
 		Logger.debug("** onStart **"); 
+		Configuration config = Configuration.root().getConfig("jongo");
+ String test = config.getString("mongodb");
+		try {
+			MorphiaObject.mongo = new Mongo("test", 27017);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 
-      
+	      
+        
+     
 		MorphiaObject.morphia = new Morphia();
-	
+		MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "test");
 		MorphiaObject.datastore.ensureIndexes();   
 		MorphiaObject.datastore.ensureCaps();  
 
